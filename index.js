85808e349172894e4a218de050a2377b933a61a0
CinthiaMonfre <cinthia.monfre@gmail.com>
2023-11-16 10:29:38 -0300

    meu primeiro cod

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 let nomeDoHeroi = "Mestre dos Magos";
-let XP = 4520;
+let XP = 5200;
 let medalha;
 
-if (XP <= 1000) {
-    medalha = "ferro";
-} else if (XP > 1000 && XP <= 2000) {
-    medalha = "Bronze";
-} else if (XP > 2000 && XP <= 5000) {
-    medalha = "Prata";
-} else if (XP > 5000 && XP <= 7000) {
-    medalha = "Ouro";
-} else if (XP > 7000 && XP <= 8000) {
-    medalha = "Platina";
-} else if (XP > 8000 && XP <= 9000) {
-    medalha = "Ascendente";
-} else if (XP > 9000 && XP <= 10000) {
-    medalha = "Imortal";
-} else { medalha = "Radiante"
-    // Adicione outros casos conforme necessário
+while (XP > 1000) {
+    if (XP <= 1000) {
+        medalha = "ferro";
+    } else if (XP <= 2000) {
+        medalha = "Bronze";
+    } else if (XP <= 5000) {
+        medalha = "Prata";
+    } else if (XP <= 7000) {
+        medalha = "Ouro";
+    } else if (XP <= 8000) {
+        medalha = "Platina";
+    } else if (XP <= 9000) {
+        medalha = "Ascendente";
+    } else if (XP <= 10000) {
+        medalha = "Imortal";
+    } else {
+        medalha = "Radiante";
+        // Adiciona outros casos conforme necessário
+    }
+    break; // Encerra o loop depois de determinar a medalha
 }
 
 console.log("O herói de nome " + nomeDoHeroi + " está no nível de " + medalha);
